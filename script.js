80782cb3a90765fd9d205aa81285f6d195b52654
EC2 Default User <ec2-user@ip-172-31-90-140.ec2.internal>
2019-10-23 21:38:07 +0000

    Final Submission

--- script.js
+++ script.js
@@ -2,16 +2,7 @@
 var app = new Vue({
   el: '#app',
   data: {
-      current:{
-        name:'',
-        catMeal:'',
-        area:'',
-        image:'',
-        instructions:'',
-        sourceLink:'',
-        youtube:'',
-        idMeal:''
-    },
+    current:{ },
     showLinks: false,
     number:0,
     addedName: '',
@@ -20,15 +11,19 @@ var app = new Vue({
     time: '',
     },
   },
+  created:{
+    getTime(){
+      this.time = moment().format('MMMM Do YYYY, h:mm:ss a');
+      return this.time;
+    }
+  },
   methods: {
-      
   async fetchREST() {
     try {
       this.showLinks = true;
       console.log("In Fetch ");
       const response = await axios.get('https://www.themealdb.com/api/json/v1/1/random.php');
       this.current = response.data.meals[0];
-
       console.log("Got Meal");
       } catch (error) {
         console.log(error);
@@ -50,43 +45,3 @@ var app = new Vue({
 
 var dt = new Date();
 document.getElementById("date").innerHTML = dt.toLocaleString();
-
-/* global fetch*/
-/*document.getElementById('button').onclick = function() {
-    var url = "https://www.themealdb.com/api/json/v1/1/random.php";
-    var name = "<br> <h2><strong>";
-    var category = "<p> Food Category: ";
-    var area = "<p> Culture: ";
-    var image = "<img src=";
-    var instructions = "<p> <br> Follow Instructions Below: <br>";
-    var youtubeLink = "<a href=";
-    var source = "<a href=";
-    var id = "<p> Meal ID: ";
-       fetch(url)
-    .then(function(reponse){
-        return reponse.json();
-    })
-    .then(function(json){
-        console.log(json);
-        for (let i = 0; i < json.meals.length; i++){
-            name += json.meals[i].strMeal + "</strong><h2>";
-            category += json.meals[i].strCategory + "</p>";
-            area += "<i>" + json.meals[i].strArea + "</i></p>";
-            image += json.meals[i].strMealThumb + ">";
-            instructions += json.meals[i].strInstructions + "</p>";
-            youtubeLink += json.meals[i].strYoutube + ">Youtube Video For Making This Meal</a><br>";
-            source += json.meals[i].strSource + ">Click Here for Ingredients, Measurements, and More Information</a>";
-            id +=json.meals[i].idMeal + "</p>";
-        }
-        document.getElementById('name').innerHTML = name;
-        document.getElementById('catMeal').innerHTML = category;
-        document.getElementById('area').innerHTML = area;
-        document.getElementById('image').innerHTML = image;
-        document.getElementById('instructions').innerHTML = instructions;
-        document.getElementById('source').innerHTML = source;
-        document.getElementById('youtube').innerHTML = youtubeLink;
-        document.getElementById('idMeal').innerHTML = id;
-        
-    });
-};
-*/
